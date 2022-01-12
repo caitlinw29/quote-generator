83297d3c1d7a594c4df3264ccdf9a44d48d19eed
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-12 15:32:46 -0500

    Add random quote on click of new quote button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-//JS
+const quoteSpan = document.getElementById("quote");
+const newQuoteBtn = document.getElementById("new-quote");
+let apiQuotes = [];
+
+//Grab Quotes from API
+async function getQuotes(){
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (err){
+    alert(err);
+  }
+}
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+  quoteSpan.textContent = quote.text;
+}
+
+newQuoteBtn.addEventListener('click', getQuotes);
